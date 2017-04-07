5f4e45b631d34cc2a87f2cccb9f6435b4f0ab4a7
cshs20120831 <cshs20120831@gmail.com>
2017-04-07 18:09:33 +0800

    正數計時

---
+++ SevSeg_Counter4.1.1.ino
@@ -0,0 +1,43 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+float Analogin;
+void setup() 
+{
+  byte numDigits = 4;
+  byte digitPins[] = {7,3,4,8};
+  byte segmentPins[] = {6,5,10,12,13,2,9,11};
+  pinMode(A0,INPUT_PULLUP);
+  pinMode(A1,INPUT_PULLUP);
+  
+  bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
+  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
+  bool updateWithDelays = false; // Default. Recommended
+  bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins,segmentPins);
+  sevseg.setBrightness(90);
+}
+
+void loop() 
+{
+  static unsigned long timer = millis();
+  static int minute=59;
+  static int hour=23;
+  
+  if(millis()>=timer)
+  {
+    if (minute == 60) 
+    { 
+      if( ++minute>=60)
+      {
+      minute=0;
+      if( ++ hour >=24)
+        hour=0;
+      }
+    } // overflow
+     sevseg.setNumber(hour * 100 + minute, 4);
+   }
+ 
+  for(int i=0;i<10;i++)
+      sevseg.refreshDisplay();
+
+}
